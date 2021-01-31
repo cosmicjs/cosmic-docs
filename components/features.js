b6239167a50d2e5ea2a606301535769e927e33de
Tony Spiro <tspiro@tonyspiro.com>
2021-01-31 11:02:13 -0600

    copy

--- components/features.js
+++ components/features.js
@@ -15,19 +15,19 @@ const features = () => (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
         </svg>
-        <h4>Powerful modeling</h4>
+        <h4>Powerful Modeling</h4>
       </div>
       <div className={styles.feature}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
         </svg>
-        <h4>Extensive tooling</h4>
+        <h4>Extensive Tooling</h4>
       </div>
       <div className={styles.feature}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
         </svg>
-        <h4>Jamstack ready</h4>
+        <h4>Jamstack Ready</h4>
       </div>
       <div className={styles.feature}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
