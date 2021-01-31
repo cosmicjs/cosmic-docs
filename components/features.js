c860ed4feb4afa86cdb24357dc92c5cc925ef535
Tony Spiro <tspiro@tonyspiro.com>
2021-01-31 09:56:59 -0600

    heroicons

--- components/features.js
+++ components/features.js
@@ -6,27 +6,41 @@ const features = () => (
     <p className="text-lg mb-2 text-gray-600 md:text-xl">Content API and developer toolkit</p>
     <div className={styles.features}>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/6174d730-898e-11ea-9edc-335682595c41-cubes.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
+        </svg>
+        &nbsp;&nbsp;
         <h4>Composable</h4>
       </div>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/50f3c410-25c1-11ea-a865-1b8d71835f52-009-stem-cells.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
+        </svg>
+        &nbsp;&nbsp;
         <h4>Powerful modeling</h4>
       </div>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/9a0e1560-3007-11ea-a774-fd983bfe489d-071-screwdriver.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+        </svg>
         <h4>Extensive tooling</h4>
       </div>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/fa4b67e0-33e3-11ea-9530-5d76cd173054-112-layers.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+        </svg>
         <h4>Jamstack ready</h4>
       </div>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/548985c0-61f6-11eb-99a3-376f095ca0cf-secure.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
+        </svg>
         <h4>Secure</h4>
       </div>
       <div className={styles.feature}>
-        <img className={styles.img} src="https://cdn.cosmicjs.com/559dad30-6574-11ea-8882-7f56d1a66c15-048-profit.svg"/>&nbsp;&nbsp;
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
+        </svg>
         <h4>Scalable</h4>
       </div>
     </div>
