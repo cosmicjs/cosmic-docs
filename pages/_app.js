68e5f980d337243774f35ccbbf199d15a1d19711
Tony Spiro <tspiro@tonyspiro.com>
2021-02-09 17:54:21 -0600

    fix gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,7 @@
 import "../styles.css";
 import "nextra-theme-docs/style.css";
 import TagManager from 'react-gtm-module'
-import { useEffect } from 'react'
-const tagManagerArgs = {
-  gtmId: process.env.NEXT_PUBLIC_GTAG
-}
 export default function Nextra({ Component, pageProps }) {
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
   return (
     <>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { SkipNavLink } from "@reach/skip-nav";
-
+const gtmId = process.env.NEXT_PUBLIC_GTAG;
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtmId}');`,
+        }}>
+        </script>
+        </Head>
         <body>
           <SkipNavLink />
           <Main />
