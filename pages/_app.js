23c892f43184fb28f5eab89467403cf68d302219
Tony Spiro <tspiro@tonyspiro.com>
2021-02-09 17:56:26 -0600

    rm gtag module

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles.css";
 import "nextra-theme-docs/style.css";
-import TagManager from 'react-gtm-module'
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
