2c4f0de1a5007cb7fec27d37aeb62ff3c8704c64
Tony Spiro <tspiro@tonyspiro.com>
2021-02-24 18:08:57 -0600

    mobile tabs

--- components/tabs.js
+++ components/tabs.js
@@ -2,28 +2,30 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { Rest, Node, GraphQL, CLI } from './icons'
 const tabs = (props) => {
   return  <Tabs>
-    <TabList>
-      {
-        props.children.map(child => {
-          const label = child.props.label;
-          let icon;
-          if (label === 'REST')
-            icon = Rest;
-          if (label === 'Node.js')
-            icon = Node;
-          if (label === 'GraphQL')
-            icon = GraphQL;
-          if (label === 'CLI')
-            icon = CLI;
-          return (
-            <Tab key={`tab-${child.props.label}`}>
-              <div className="label-icon">{icon}</div>
-              <div className="label-text">{label}</div>
-            </Tab>
-          )
-        })
-      }
-    </TabList>
+    <div className="tablist-wrapper">
+      <TabList>
+        {
+          props.children.map(child => {
+            const label = child.props.label;
+            let icon;
+            if (label === 'REST')
+              icon = Rest;
+            if (label === 'Node.js')
+              icon = Node;
+            if (label === 'GraphQL')
+              icon = GraphQL;
+            if (label === 'CLI')
+              icon = CLI;
+            return (
+              <Tab key={`tab-${child.props.label}`}>
+                <div className="label-icon">{icon}</div>
+                <div className="label-text">{label}</div>
+              </Tab>
+            )
+          })
+        }
+      </TabList>
+    </div>
     {
       props.children.map(child => <TabPanel key={`panel-${child.props.label}`}>{child}</TabPanel>)
     }
