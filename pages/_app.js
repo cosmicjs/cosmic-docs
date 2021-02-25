a0d84194105d5249e6c64b3594186ee749db45c5
Tony Spiro <tspiro@tonyspiro.com>
2021-02-24 19:03:11 -0600

    fix 404

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles.css";
 import "nextra-theme-docs/style.css";
+import FourOhFour from "./_404"
 
 export default function Nextra({ Component, pageProps }) {
+  if (pageProps.statusCode === 404) {
+    return <FourOhFour />
+  }
   return (
     <>
       <Component {...pageProps} />
