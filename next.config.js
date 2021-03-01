745bc7a554ccc455712ec07228061dcf93c0821d
Tony Spiro <tspiro@tonyspiro.com>
2021-03-01 09:04:38 -0600

    fix route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
-const path = process.env.NODE_ENV === 'development' ? './nextra-theme-docs/src' : 'nextra-theme-docs';
+const path = process.env.NODE_ENV === 'development' ? './nextra-theme-docs/src' : './nextra-theme-docs';
 const withNextra = require("nextra")(path, "./theme.config.js");
 module.exports = withNextra();
