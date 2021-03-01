cc4cc53ec9c1bccf9df80b6cf55f7ee5b5580aae
Tony Spiro <tspiro@tonyspiro.com>
2021-02-28 22:10:53 -0600

    added local theme

--- next.config.js
+++ next.config.js
@@ -1,2 +1,3 @@
-const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js");
+const folder = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
+const withNextra = require("nextra")(`./nextra-theme-docs/${folder}`, "./theme.config.js");
 module.exports = withNextra();
